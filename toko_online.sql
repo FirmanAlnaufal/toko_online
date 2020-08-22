dc263edcca3abccccc2446edaa59c7654b3d09b4
FirmanAlnaufal <alnaufal96@gmail.com>
2020-08-22 20:15:40 +0700

    database

---
+++ toko_online.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 22, 2020 at 02:46 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_brg` varchar(50) NOT NULL,
+  `keterangan` varchar(50) NOT NULL,
+  `kategori` varchar(60) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `gambar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id_barang`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
+(1, 'sepatu', 'ALL STAR', 'pakaian pria', 300000, 39, 'sepatu1.jpg'),
+(2, 'laptop', 'ASUS intel core i9', 'elektronik', 4000000, 10, 'laptop.jpg'),
+(3, 'kamera', 'canon 720', 'elektronik', 2000000, 20, 'kamera.jpg'),
+(4, 'Hp Xiaomi', 'xiomi Note 4 snapdragon 980', 'elektronik', 5500000, 20, 'hp.jpg'),
+(6, 'jam tangan', 'jam tangan classic', 'pakaian pria', 400000, 20, 'jam.jpg'),
+(7, 'kemeja', 'kemeja kantoran casual ', 'pakaian pria', 200000, 20, 'kemeja.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_invoice`
+--
+
+CREATE TABLE `tb_invoice` (
+  `id_invoice` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `tgl_pesan` datetime NOT NULL,
+  `batas_bayar` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_invoice`
+--
+
+INSERT INTO `tb_invoice` (`id_invoice`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`) VALUES
+(1, 'amelia rohmana', 'bindung city', '2020-03-13 14:29:14', '2020-03-14 14:29:14'),
+(2, 'firman alnaufal', 'lebux city', '2020-03-13 14:46:44', '2020-03-14 14:46:44'),
+(3, 'Asep', 'Jakarta', '2020-03-13 22:10:34', '2020-03-14 22:10:34'),
+(4, '', '', '2020-07-21 13:03:36', '2020-07-22 13:03:36'),
+(5, '', '', '2020-07-21 14:36:46', '2020-07-22 14:36:46'),
+(6, '', '', '2020-07-21 14:44:06', '2020-07-22 14:44:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pesanan`
+--
+
+CREATE TABLE `tb_pesanan` (
+  `id` int(11) NOT NULL,
+  `id_invoice` int(11) NOT NULL,
+  `id_brg` int(11) NOT NULL,
+  `nama_brg` varchar(50) NOT NULL,
+  `jumlah` int(3) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `pilihan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_pesanan`
+--
+
+INSERT INTO `tb_pesanan` (`id`, `id_invoice`, `id_brg`, `nama_brg`, `jumlah`, `harga`, `pilihan`) VALUES
+(1, 1, 1, 'sepatu', 1, 300000, ''),
+(2, 1, 2, 'laptop', 1, 4000000, ''),
+(3, 1, 3, 'kamera', 1, 2000000, ''),
+(4, 2, 2, 'laptop', 1, 4000000, ''),
+(5, 2, 7, 'kemeja', 1, 200000, ''),
+(6, 3, 1, 'sepatu', 1, 300000, ''),
+(7, 3, 2, 'laptop', 2, 4000000, ''),
+(8, 3, 3, 'kamera', 1, 2000000, ''),
+(9, 4, 2, 'laptop', 1, 4000000, ''),
+(11, 6, 1, 'sepatu', 1, 300000, '');
+
+--
+-- Triggers `tb_pesanan`
+--
+DELIMITER $$
+CREATE TRIGGER `pesanan_penjualan` AFTER INSERT ON `tb_pesanan` FOR EACH ROW BEGIN
+	UPDATE tb_barang SET stok = stok-NEW.jumlah
+    WHERE id_barang = NEW.id_brg;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `tb_invoice`
+--
+ALTER TABLE `tb_invoice`
+  ADD PRIMARY KEY (`id_invoice`);
+
+--
+-- Indexes for table `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_invoice`
+--
+ALTER TABLE `tb_invoice`
+  MODIFY `id_invoice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
